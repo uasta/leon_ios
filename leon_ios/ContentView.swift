2e179334491e70567a89b8cc7cfd2ebe40949853
leon <1275890441@qq.com>
2026-04-02 15:37:13 +0800

    Initial Commit

---
+++ leon_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  leon_ios
+//
+//  Created by 胡亮 on 2026/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ leon_ios/leon_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  leon_iosApp.swift
+//  leon_ios
+//
+//  Created by 胡亮 on 2026/4/2.
+//
+
+import SwiftUI
+
+@main
+struct leon_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
